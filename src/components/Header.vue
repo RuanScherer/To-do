a51619c4bff1b88f1e3552ecdb1f1a017f710466
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-04-27 15:40:06 -0300

    Header created

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-blue-grey darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>To do</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      fixed
+      elevate-on-scroll
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    drawer: false
+  })
+}
+</script>
