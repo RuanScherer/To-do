049fa6995c997e4ce1b0d2c4d6b50e6f5d910710
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-04-27 15:41:59 -0300

    Aplication views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="d-flex flex-wrap">
+    <v-col cols="6" class="pa-2">
+      <v-card elevation="2" class="pa-2">
+        <v-card-title>Projeto</v-card-title>
+        <v-card-subtitle>Subtítulo</v-card-subtitle>
+        <v-card-text>Descrição</v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="blue-grey darken-3"
+          >
+            Learn More
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="6" class="pa-2">
+      <v-card elevation="2" class="pa-2">
+        <v-card-title>Projeto</v-card-title>
+        <v-card-subtitle>Subtítulo</v-card-subtitle>
+        <v-card-text>Descrição</v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="blue-grey darken-3"
+          >
+            Learn More
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="6" class="pa-2">
+      <v-card elevation="2" class="pa-2">
+        <v-card-title>Projeto</v-card-title>
+        <v-card-subtitle>Subtítulo</v-card-subtitle>
+        <v-card-text>Descrição</v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="blue-grey darken-3"
+          >
+            Learn More
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col cols="6" class="pa-2">
+      <v-card elevation="2" class="pa-2">
+        <v-card-title>Projeto</v-card-title>
+        <v-card-subtitle>Subtítulo</v-card-subtitle>
+        <v-card-text>Descrição</v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="blue-grey darken-3"
+          >
+            Learn More
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+}
+</script>
---
+++ src/views/Project.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column">
+    <v-card elevation="2" class="pa-2 w-100">
+      <v-card-title>Projeto</v-card-title>
+      <v-card-subtitle>Subtítulo</v-card-subtitle>
+      <v-card-text>Descrição</v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="blue-grey darken-3"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project"
+}
+</script>
+
+<style>
+
+</style>
