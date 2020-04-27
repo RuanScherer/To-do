c605fb13d943e18ad84143a95f593b32c948c3a1
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-04-27 15:40:56 -0300

    Router added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import Dashboard from './views/Dashboard.vue'
+import Project from './views/Project.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { 
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  { 
+    path: '/project',
+    name: 'Project',
+    component: Project
+  }
+]
+
+const router = new VueRouter({
+  routes: routes
+ })
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
