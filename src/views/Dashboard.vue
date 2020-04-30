5168dd345e0b3337095b9198b2107a7f54ad1ddf
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-04-30 15:39:47 -0300

    Adapted to bootstrap

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="d-flex flex-wrap">
-    <v-col v-for="project in projects" :key="project.id" cols="4" class="pa-2">
-      <v-card elevation="2" class="pa-2">
-        <v-card-title>{{ project.name }}</v-card-title>
-        <v-card-text>{{ project.description }}</v-card-text>
-        <v-card-actions>
-          <v-btn
-            text
-            color="blue-grey darken-3"
-            :to="'/project/' + project.id"
-          >
-            Gerenciar
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
+  <div class="container py-4">
+    <section class="card shadow-sm radius">
+      <div class="card-body text-center">
+        <h3 class="card-title">Olá, Ruan</h3>
+        <h5 class="card-subtitle text-secondary">Vamos trabalhar em qual projeto agora?</h5>
+      </div>
+    </section>
+    <a class="btn btn-primary rounded-pill btn-primary-smoof w-100 my-3" href="/project/new">Novo projeto</a>
+    <section class="card shadow-sm radius">
+      <div class="card-body d-flex flex-wrap">
+        <article 
+          class="col-md-4 p-3" 
+          v-for="project in projects" 
+          :key="project.id" 
+          :href="/project/ + project.id">
+          <div class="card card-elevate radius shadow-sm">
+            <div class="card-body text-center">
+              <h3 class="card-title">{{ project.name }}</h3>
+              <h5 class="card-subtitle text-secondary">{{ project.description }}</h5>
+              <a class="btn btn-primary rounded-pill align-self-center mt-3" :href="/project/ + project.id">Entrar no projeto</a>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
   </div>
 </template>
 
