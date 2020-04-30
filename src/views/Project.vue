a6ad69591fb7326fe1ec3b712207995e52394ac3
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-04-30 16:46:12 -0300

    Adapted to bootstrap

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,27 +1,62 @@
 <template>
-  <div class="d-flex flex-column">
-    <v-card elevation="2" class="pa-2 w-100">
-      <v-card-title>Projeto</v-card-title>
-      <v-card-subtitle>Subtítulo</v-card-subtitle>
-      <v-card-text>Descrição</v-card-text>
-      <v-card-actions>
-        <v-btn
-          text
-          color="blue-grey darken-3"
-        >
-          Learn More
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <div class="container py-4">
+    <section class="card shadow-sm radius">
+      <div class="card-body text-center">
+        <h3 class="card-title">{{ project.name }}</h3>
+        <h5 class="card-subtitle text-secondary">{{ project.description }}</h5>
+      </div>
+    </section>
+    <section class="card shadow-sm radius mt-3">
+      <div class="card-body d-flex flex-wrap">
+        <router-link class="btn btn-primary rounded-pill btn-primary-smoof w-100 mx-3 mb-2" to="/task/new">Nova tarefa</router-link>
+        <article 
+          class="col-md-4 p-3" 
+          v-for="task in tasks" 
+          :key="task.id">
+          <div class="card card-elevate radius">
+            <div class="card-body text-center">
+              <h3 class="card-title">{{ task.name }}</h3>
+              <h5 class="card-subtitle text-secondary mb-2">{{ task.description }}</h5>
+              <router-link class="btn btn-primary rounded-pill align-self-center m-1" :to="/task/ + task.id">Marcar como concluído</router-link>
+              <router-link class="btn btn-info rounded-pill align-self-center m-1" :to="/task/ + task.id">Editar</router-link>
+              <router-link class="btn btn-danger rounded-pill align-self-center m-1" :to="/task/ + task.id">Remover</router-link>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: "Project"
+  name: "Project",
+  props: ['id'],
+  data: () => {
+    return {
+      project: null,
+      tasks: null,
+    }
+  },
+  methods: {
+    remove: async (evt) => {
+      console.log(evt)
+      //await axios.delete(`http://localhost:8000/api/tasks/${this.id}`)
+      //  .then( (response) => { this.tasks = response.data.task } )
+      //  .catch( (error) => { this.tasks = error } )
+    }
+  },
+  async mounted() {
+    await axios.get(`http://localhost:8000/api/projects/${this.id}`)
+      .then( async (response) => {
+        this.project = response.data.project
+        await axios.get(`http://localhost:8000/api/tasks/project/${this.id}`)
+          .then( (response) => { this.tasks = response.data.task } )
+          .catch( (error) => { this.tasks = error } )
+      } )
+      .catch( (error) => { this.project = error } )
+  }
 }
 </script>
-
-<style>
-
-</style>
