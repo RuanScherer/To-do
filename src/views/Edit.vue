43dea94d7b94000e63f3fa5dbdee75a4c0edb224
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-15 11:23:41 -0300

    Created base component for 'edit' pages

---
+++ src/views/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container py-4">
+    <MainCard title="Estamos em constante mudança..." subtitle="Não se preocupe, entendemos que é normal"></MainCard>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MainCard from '../components/MainCard.vue'
+
+export default {
+  name: "Edit",
+  components: {
+    MainCard
+  }
+}
+</script>
