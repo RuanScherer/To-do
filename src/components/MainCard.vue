f7868800f7df6694f07573e26adb16361e642b51
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-15 11:01:40 -0300

    Optimized to use slots

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -3,13 +3,7 @@
     <div class="card-body text-center">
       <h3 class="card-title">{{ title }}</h3>
       <h5 class="card-subtitle text-secondary mb-2">{{ subtitle }}</h5>
-      <router-link 
-        v-for="button in buttons" 
-        :key="button.title" 
-        :class="'btn btn-' + button.theme + '-smoof no-elevate rounded-pill align-self-center m-1'" 
-        :to="button.link + id">
-        {{ button.title }}
-      </router-link>
+      <slot name="buttons"></slot>
     </div>
   </section>
 </template>
