6458d95b6fa76dcc6d95a62e59635e4956a1f50f
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-15 09:30:51 -0300

    Change request to cloud api

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,16 +3,16 @@
     <MainCard title="Olá, Ruan" subtitle="Em qual projeto vamos trabalhar agora?"></MainCard>
     <section class="card shadow-sm radius mt-3">
       <div class="card-body d-flex flex-wrap">
-        <router-link class="btn btn-primary rounded-pill btn-primary-smoof no-elevate w-100 mx-3 mb-2" to="/project/new">Novo projeto</router-link>
+        <router-link class="btn btn-primary rounded-pill btn-primary-smoof no-elevate w-100 mx-3 mb-2" to="/new/project">Novo projeto</router-link>
         <article 
           class="col-md-4 p-3" 
           v-for="project in projects" 
-          :key="project.id">
+          :key="project._id">
           <div class="card card-elevate radius">
             <div class="card-body text-center">
               <h3 class="card-title">{{ project.name }}</h3>
               <h5 class="card-subtitle text-secondary">{{ project.description }}</h5>
-              <router-link class="btn btn-primary rounded-pill align-self-center mt-3" :to="/project/ + project.id">Entrar no projeto</router-link>
+              <router-link class="btn btn-primary rounded-pill align-self-center mt-3" :to="/overview/ + project._id">Entrar no projeto</router-link>
             </div>
           </div>
         </article>
@@ -36,7 +36,7 @@ export default {
     }
   },
   async mounted() {
-    await axios.get('http://localhost:8000/api/projects')
+    await axios.get('https://ruanscherer-todo.herokuapp.com/project/all')
       .then( response => { this.projects = response.data.projects } )
       .catch( (error) => { this.projects = error } )
   }
