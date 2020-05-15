8f1e3991d45873c43a64e4402b6eb04b723fa158
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-15 09:32:41 -0300

    Default component for 'register' pages

---
+++ src/views/Create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container py-4">
+    <MainCard title="Vamos criar algo novo?" subtitle="É bom ver que você está sempre ativo :)"></MainCard>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MainCard from '../components/MainCard.vue'
+
+export default {
+  name: "Create",
+  components: {
+    MainCard
+  }
+}
+</script>
